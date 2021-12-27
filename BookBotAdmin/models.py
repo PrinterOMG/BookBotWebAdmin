201683f81519f041579059fc983b379d5d553132
PrinterOMG <PrinterOMG@ya.ru>
2021-12-27 12:24:29 +0400

    * Initial commit

---
+++ BookBotAdmin/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+
+class Users(models.Model):
+    pass
+
+
+class Promocodes(models.Model):
+    pass
+
+
+class Questions(models.Model):
+    pass
+
+
+class Stats(models.Model):
+    pass
+
+
+class Subscribes(models.Model):
+    pass
+
+
+class Filters(models.Model):
+    pass
+
+
+class Mailing(models.Model):
+    pass
+
+
+class Posts(models.Model):
+    pass
+
+
+class Books(models.Model):
+    pass
+
+
+class SubPrices(models.Model):
+    pass
+
+
+class Languages(models.Model):
+    pass

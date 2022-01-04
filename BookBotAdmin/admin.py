32e0dae578cead51fef7f2c09cda6d3a3aacda4d
PrinterOMG <PrinterOMG@ya.ru>
2022-01-04 18:58:00 +0400

    * Fix admin

--- BookBotAdmin/admin.py
+++ BookBotAdmin/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-import models
+from . import models
 
 
 class UsersAdmin(admin.ModelAdmin):
     list_display = ("userId", "balance", "referral", "isBlock", "languageId", "showProgress")
-    list_display_links = ("userId")
-    search_fields = ("userId")
-    list_filter = ("balance", "referral", "isblock")
+    list_display_links = ["userId"]
+    search_fields = ["userId"]
+    # list_filter = ("balance", "referral", "isblock")
 
 
 admin.site.register(models.Users, UsersAdmin)

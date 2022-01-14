cdeba02393cf840852cf895384d4605cf4c55dc6
PrinterOMG <PrinterOMG@ya.ru>
2022-01-14 15:21:22 +0400

    * Update models

--- BookBotAdmin/models.py
+++ BookBotAdmin/models.py
@@ -12,6 +12,7 @@ class Users(models.Model):
     deposit = models.IntegerField(default=0, verbose_name="Депозит")
     subscribeTime = models.IntegerField(default=0, verbose_name="Месяцы подписки")
     notEndPayment = models.BooleanField(verbose_name="Не закончил оплату")
+    paymentId = models.CharField(max_length=128, verbose_name="Сохраненный способ оплаты", blank=True, null=True)
 
     def __repr__(self):
         return self.username

549afb1a94d01d0159f35cf0ad88b04ec4d67d03
PrinterOMG <PrinterOMG@ya.ru>
2022-01-20 21:39:12 +0400

    * Release

--- BookBotAdmin/admin.py
+++ BookBotAdmin/admin.py
@@ -142,10 +142,8 @@ class Statistic(admin.ModelAdmin):
 
     fund_books.short_description = "Книги"
 
-    # def has_add_permission(self, request):
-    #     return False
-    #
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
+    def has_add_permission(self, request):
+        return False
 
-# admin.site.register(models.Mailing)
+    def has_delete_permission(self, request, obj=None):
+        return False

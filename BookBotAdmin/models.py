91ed50f39144941bbeb26c4bdbb36f96d3cdaa0b
cmdrBebop <kostyadevyatov@gmail.com>
2022-01-22 12:20:20 +0400

    Fix posts

--- BookBotAdmin/models.py
+++ BookBotAdmin/models.py
@@ -132,7 +132,7 @@ class Posts(models.Model):
     text = models.TextField(verbose_name="Текст")
     photo = models.ImageField(upload_to="imgs/posts/", verbose_name="Картинка", blank=True, null=True)
     date = models.DateTimeField(verbose_name="Дата", auto_now=True)
-    filter = models.ForeignKey("Filters", on_delete=models.CASCADE, verbose_name="Фильтр", blank=True)
+    filter = models.ForeignKey("Filters", on_delete=models.CASCADE, verbose_name="Фильтр", blank=True, null=True)
     isSend = models.BooleanField(verbose_name="Отправлен ли", default=False)
 
     def __repr__(self):
